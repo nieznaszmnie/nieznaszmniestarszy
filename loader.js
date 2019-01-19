e18f87104bb868040c98b62101c04eb0a2331b3f
nieznaszmnie <46851262+nieznaszmnie@users.noreply.github.com>
2019-01-19 21:29:43 +0100

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,1 +1,1 @@
-javascript:void((function(){var l='https://cdn.jsdelivr.net/gh/nieznaszmnie/nieznaszmnie/nowe.min.js?', r=Math.round(Math.random()*1e7),s=document.createElement('script');s.setAttribute('type', 'text/javascript');s.setAttribute('src',l+r);document.body.appendChild(s)})())
+javascript:void((function(){var l='https://cdn.jsdelivr.net/gh/nieznaszmnie/nieznaszmniestarszy/stare.min.js?', r=Math.round(Math.random()*1e7),s=document.createElement('script');s.setAttribute('type', 'text/javascript');s.setAttribute('src',l+r);document.body.appendChild(s)})())
